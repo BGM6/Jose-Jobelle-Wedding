efe1328f023fce34c4da1f08b99215f0000dd771
Bryan <bryanjguilas@gmail.com>
2021-08-01 14:54:18 -0700

    commit

--- src/components/WhereWhen.js
+++ src/components/WhereWhen.js
@@ -2,11 +2,10 @@ import React from 'react';
 import GoogleMap from './GoogleMap';
 
 const WhereWhen = () => {
-
 	const location = {
 		address: '8500 Clubhouse Dr., Pleasanton, CA 94566',
 		lat: 37.62,
-		lng: -121.85
+		lng: -121.86
 	};
 
 	return (
