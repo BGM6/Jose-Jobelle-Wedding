eb90bd790421371d2e6e60c5c0ced98d153c8936
Bryan <bryanjguilas@gmail.com>
2021-08-01 14:23:51 -0700

    api key

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -11,7 +11,7 @@ const GoogleMap = ({location, zoomLevel}) => {
 				 className="google-map"
 		>
 			<GoogleMapReact
-				bootstrapURLKeys={{key: process.env.REACT_APP_GOOGLE_API_KEY}}
+				bootstrapURLKeys={{key: 'AIzaSyC459_w1PrrsvGTANOX7xx5Uan3wh4Hrmk'}}
 				defaultCenter={location}
 				defaultZoom={zoomLevel}
 			>
